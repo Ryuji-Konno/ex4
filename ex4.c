4ac0af347007b1b4146106cab83a20c0285f299d
金野龍史 <konnoryuji@Ryuji.local>
2020-08-04 13:01:26 +0900

    HoT: Initial revision

---
+++ ex4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void){
+  int i;
+  int hc=0;
+  int tc=0;
+  char s[]="Heads";
+  char t[]="Tails";
+  int a;
+  srand(time(NULL));
+  printf("Tossing a coin...\n");
+  for(i=0;i<3;i++){
+     a = rand()%2 + 1;
+     if(a==1){
+      printf("Round %d: %s\n",i+1,s);
+      hc++;
+     }
+      else if(a==2){
+      printf("Round %d: %s\n",i+1,t);
+      tc++;
+      }
+  }
+     printf("Heads: %d, Tails: %d",hc,tc);
+  return 0;
+}
